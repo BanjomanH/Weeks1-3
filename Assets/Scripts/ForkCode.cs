cbc226491746ff305c662b6e968966d27b3e272b
Ben Hicken <hickenb@sheridancollege.ca>
2025-01-29 15:38:11 -0500

    Implemented all of the needed code to have the hand work as intended. Bug fixing needs to happen to make sure the checkArea method works as intended.

---
+++ Assets/Scripts/ForkCode.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ForkCode : MonoBehaviour
+{
+    public Transform handPos;
+    public Vector3 pastaPos;
+    public Vector3 mouthPos;
+    public float hitbox;
+    public bool spaghetti = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        cursor.z = 0;
+
+        handPos.position = cursor;
+
+        if (checkArea(pastaPos))
+        {
+            spaghetti = true;
+        }
+        if (checkArea(mouthPos))
+        {
+            spaghetti = false;
+        }
+
+        if (spaghetti == true)
+        {
+            transform.localScale = Vector3.one * 1.5f;
+        } else
+        {
+            transform.localScale = Vector3.zero;
+        }
+    }
+    bool checkArea(Vector3 area)
+    {
+        bool isInArea = false;
+
+        if (transform.position.x > area.x - hitbox && transform.position.x < area.x + hitbox)
+        {
+            print("reached!");
+            if (transform.position.y > area.y - hitbox && transform.position.y < area.y + hitbox)
+            {
+                print("done!");
+                isInArea = true;
+            }
+        }
+
+        return isInArea;
+    }
+}
